8baf4376b8b3609285d86d9f962da342f73504a5
Jardo <schuppjarod@gmail.com>
2023-10-05 21:04:01 -0400

    term_list_item & Term Adapter textView name seperate causing crashes

--- app/src/main/java/com/example/studenttracker/ui/TermList.java
+++ app/src/main/java/com/example/studenttracker/ui/TermList.java
@@ -63,10 +63,22 @@ public class TermList extends AppCompatActivity {
             repository.insert(term);
             Term term2 = new Term(2, "Second Term", "01/1/23", "12/31/23");
             repository.insert(term2);
+            Term term3 = new Term(3, "Third Term", "01/1/23", "12/31/23");
+            repository.insert(term3);
+            Term term4 = new Term(4, "Fourth Term", "01/1/23", "12/31/23");
+            repository.insert(term4);
+
+
+
             Classes classes = new Classes(1, "Math Classes", "01/1/23", "12/31/23", "Almost Done", "Jane Doe", "1234567890", "test@test.org", 1, "Note Example");
             repository.insert(classes);
             Classes classes2 = new Classes(2, "Science Classes", "01/2/23", "12/30/23", "Almost Done", "Jane Doe", "1234567890", "test@test.org", 2, "Note Example");
             repository.insert(classes2);
+            Classes classes3 = new Classes(3, "English Classes", "01/3/23", "12/29/23", "Almost Done", "Jane Doe", "1234567890", "test@test.org" , 3, "Note Example");
+            repository.insert(classes3);
+            Classes classes4 = new Classes(4, "History Classes", "01/4/23", "12/28/23", "Almost Done", "Jane Doe", "1234567890", "test@test.org", 4, "Note Example");
+            repository.insert(classes4);
+
             return true;
         }
         if (item.getItemId() == android.R.id.home) {
